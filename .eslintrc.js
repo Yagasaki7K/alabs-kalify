6231cfbb47b0d7cacc1a73ab2d8192381e5e960d
Anderson Marlon <anderson18.marlon@gmail.com>
2023-03-14 23:17:45 -0300

    Restore: Codes

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,22 +1,18 @@
+// eslint-disable-next-line no-undef
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true
+    env: {
+        browser: true,
+        es2021: true,
     },
-    "extends": [
-        "eslint:recommended",
-        "plugin:react/recommended"
-    ],
-    "overrides": [
-    ],
-    "parserOptions": {
-        "ecmaVersion": "latest",
-        "sourceType": "module"
+    extends: ["eslint:recommended", "plugin:react/recommended"],
+    overrides: [],
+    parserOptions: {
+        ecmaVersion: "latest",
+        sourceType: "module",
     },
-    "plugins": [
-        "react"
-    ],
-    "rules": {
-        "react/react-in-jsx-scope": "off"
-    }
-}
+    plugins: ["react"],
+    rules: {
+        "react/react-in-jsx-scope": "off",
+        "react/prop-types": "off",
+    },
+};
