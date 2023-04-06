bf5659ebddf347747fa106e7651110e62ce0c5f0
Anderson Marlon <anderson18.marlon@gmail.com>
2023-04-06 11:38:47 -0300

    [Dashboard] Slug

--- pages/[slug].jsx
+++ pages/[slug].jsx
@@ -6,113 +6,47 @@ import Header from '../src/components/Header'
 import Footer from '../src/components/Footer'
 import SlugDetails from '../src/components/SlugDetails'
 import postService from '../services/post.service'
-import { async } from '@firebase/util'
 
-export async function getStaticProps() {
-    const response = await fetch('https://raw.githubusercontent.com/Yagasaki7K/website-onigirihardcore/main/server/index.json')
-    const data = await response.json()
-    return {
-        props: {
-            data
-        }
-    }
-}
+// export async function getStaticProps() {
+//     const response = await fetch('https://raw.githubusercontent.com/Yagasaki7K/website-onigirihardcore/main/server/index.json')
+//     const data = await response.json()
+//     return {
+//         props: {
+//             data
+//         }
+//     }
+// }
 
-export async function getStaticPaths() {
-    const request = await fetch('https://raw.githubusercontent.com/Yagasaki7K/website-onigirihardcore/main/server/index.json')
-    const posts = await request.json()
-    const paths = posts.map(client => ({
-        params: { slug: client.slug.toString() },
-    }))
+// export async function getStaticPaths() {
+//     const request = await fetch('https://raw.githubusercontent.com/Yagasaki7K/website-onigirihardcore/main/server/index.json')
+//     const posts = await request.json()
+//     const paths = posts.map(client => ({
+//         params: { slug: client.slug.toString() },
+//     }))
 
-    return {
-        paths,
-        fallback: false
-    }
-}
+//     return {
+//         paths,
+//         fallback: false
+//     }
+// }
 
 // Test using Localhost || Hidde getStaticPaths and getStaticProps and props inside on Post
 // import data from '../server/index.json'
 
 // eslint-disable-next-line react/prop-types
-/*const Post = ({ data }) => {
+const Post = () => {
     const router = useRouter()
     const { slug } = router.query
 
-    return (
-        <>
-            <Header />
-
-            <SlugDetails>
-                {
-                    // eslint-disable-next-line react/prop-types
-                    data && data.map((post, index) => (
-                        post.slug === slug ? (
-                            <div key={index}>
-                                <Head>
-                                    <title>{post.title}</title>
-                                    <meta name="author" content='Anderson "Yagasaki" Marlon' />
-                                    <meta name="description" content={post.description} />
-                                    <meta property="og:title" content={post.title} />
-                                    <meta property="og:description" content={post.description} />
-                                    <meta property="og:site_name" content="Anderson Marlon // Software Developer" />
-                                    <meta property="og:url" content="https://yagasaki.vercel.app/" />
-                                    <meta property="og:image" content={post.image} />
-                                    <meta property="og:type" content="Website" />
-                                    <meta property="og:image:width" content="1200" />
-                                    <meta property="og:image:height" content="627" />
-                                    <meta name="twitter:card" content="summary" />
-                                    <meta name="twitter:site" content={post.title} />
-                                    <meta name="twitter:creator" content='Anderson "Yagasaki" Marlon' />
-                                    <meta name="twitter:title" content={post.title} />
-                                    <meta name="twitter:description" content={post.description} />
-                                    <meta name="twitter:image:src" content={post.image} />
-                                </Head>
-
-                                <img src={post.image}></img>
-                                <section key={post.id}>
-                                    <p className="block__content">{post.createdAtExtended} | {post.author}</p>
-                                    <h1 className="title__content">{post.title}</h1>
-                                    <p className="block__content" style={{ whiteSpace: "pre-wrap" }}>{post.body}</p>
-
-                                    {
-                                        post.citation != '' ? (
-                                            <a href={post.linkCitation} className="citation" target="_blank" rel="noreferrer">
-                                                <p className="block__content">“{post.citation}”</p>
-                                            </a>
-                                        ) : null
-                                    }
-
-                                    <p className="block__content" style={{ whiteSpace: "pre-wrap" }}>{post.body2}</p>
-
-                                    {
-                                        post.ytid ?
-                                            <iframe width="550" height="480" src={'https://www.youtube.com/embed/' + post?.ytid} frameBorder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe> : null
-                                    }
-                                </section>
-                            </div>
-                        ) : (
-                            null
-                        )
-                    ))
-                }
-            </SlugDetails>
-            <Footer />
-        </>
-    )
-}
-export default Post*/
-
-function Post(){
     const [Post, setPost] = useState([])
 
-    useEffect(()=>{
+    useEffect(() => {
         getPost()
     }, [])
 
     const getPost = async () => {
         const data = await postService.getAllPosts()
-        setPost(data.docs.map((doc)=> ({...doc.data(), id: doc.id})))
+        setPost(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })))
     }
 
     const firebaseURL = 'https://firebasestorage.googleapis.com/v0/b/onigirihardcore-88090.appspot.com/o/files%2F'
@@ -147,11 +81,14 @@ function Post(){
                                     <meta name="twitter:image:src" content={post.image} />
                                 </Head>
 
+                                {/* <img src={post.image}></img> */}
                                 <img src={post.image ? firebaseURL + post.image + `?alt=media` : null} alt={post?.name}></img>
                                 <section key={post.id}>
-                                    <p className="block__content">{post.createdAtExtended} | {post.author}</p>
+                                    <p className="block__content">{post.moreDate} | {post.author}</p>
+                                    {/* <p className="block__content">{post.createdAtExtended} | {post.author}</p> */}
                                     <h1 className="title__content">{post.title}</h1>
-                                    <p className="block__content" style={{ whiteSpace: "pre-wrap" }}>{post.body}</p>
+                                    <p className="block__content" style={{ whiteSpace: "pre-wrap" }}>{post.bodyPost}</p>
+                                    {/* <p className="block__content" style={{ whiteSpace: "pre-wrap" }}>{post.body}</p> */}
 
                                     {
                                         post.citation != '' ? (
@@ -161,7 +98,7 @@ function Post(){
                                         ) : null
                                     }
 
-                                    <p className="block__content" style={{ whiteSpace: "pre-wrap" }}>{post.body2}</p>
+                                    {/* <p className="block__content" style={{ whiteSpace: "pre-wrap" }}>{post.body2}</p> */}
 
                                     {
                                         post.ytid ?
